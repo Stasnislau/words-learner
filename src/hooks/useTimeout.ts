3042a963094b1a8b4ed52df5468681160dfdb219
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-12-10 20:09:35 +0100

    fixed timeout

--- src/hooks/useTimeout.ts
+++ src/hooks/useTimeout.ts
@@ -8,11 +8,33 @@ const useTimeout = (
   callBack: (props?: any) => any
 ) => {
   const [time, setTime] = useState(initialTime);
+  const hasDelayFinished = useRef(false);
   const isRunning = useRef(startImmediately);
-  const callbackRef = useRef(callBack); 
+  const callbackRef = useRef(callBack);
+
+  // the delay should be applied only once, when the component mounts
+  // so we need to check if the delay has already finished
+
+  const calculateTimer = () => {
+    if (!hasDelayFinished.current) return delay;
+    return 1000;
+  };
+
   useEffect(() => {
     callbackRef.current = callBack;
   }, [callBack]);
+
+  useEffect(() => {
+    if (delay > 0 && isRunning.current) {
+      const delayTimer = setTimeout(() => {
+        hasDelayFinished.current = true;
+        setTime(initialTime);
+        isRunning.current = true;
+      }, delay);
+      return () => clearTimeout(delayTimer);
+    }
+    return () => {};
+  }, [delay, initialTime, startImmediately]);
 
   useEffect(() => {
     if (isRunning.current) setTime(initialTime);
@@ -25,15 +47,12 @@ const useTimeout = (
       return;
     }
     if (!isRunning.current) return;
-    const timer = setTimeout(
-      () => setTime(time - 1),
-      time === initialTime ? delay : 1000
-    );
+    const timer = setTimeout(() => setTime(time - 1), calculateTimer());
     return () => clearTimeout(timer); // clear the timeout when the component unmounts
-  }, [time, isRunning.current, isRunning, initialTime, delay]);
+  }, [time, isRunning.current, initialTime, delay, hasDelayFinished.current]);
   const setIsRunning = (value: boolean) => {
     isRunning.current = value;
-  }
+  };
   return [time, setIsRunning] as const;
 };
 
