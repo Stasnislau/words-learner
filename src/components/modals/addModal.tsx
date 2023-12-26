a00d27d770d65c8a268e2c538d1b1836e4b9c0f6
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-12-26 18:26:09 +0100

    included addModal

--- src/components/modals/addModal.tsx
+++ src/components/modals/addModal.tsx
@@ -1,81 +1,115 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { topic, wordCard } from "../../types";
+import { availableTopics } from "../../constants";
+import useClickOutside from "../../hooks/useClickOutside";
+
+import { motion } from "framer-motion";
 
 type ModalProps = {
-  isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
-  item: wordCard;
-  setItem: (item: wordCard) => void;
 };
 
-const AddModal  = ({ isOpen, setIsOpen, item, setItem }: ModalProps) => {
-  const [modifiedWord, setModifiedWord] = useState(item.word);
-  const [modifiedTranslation, setModifiedTranslation] = useState(item.translation);
+const AddModal = ({ setIsOpen }: ModalProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [modifiedWord, setModifiedWord] = useState("");
+  const [modifiedTranslation, setModifiedTranslation] = useState("");
   const [topic, setTopic] = useState({} as topic);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [numberClicked, setNumberClicked] = useState<number>(0);
+  const wordsList = useRef<wordCard[]>([]);
+  useEffect(() => {
+    if (localStorage.getItem("wordList")) {
+      wordsList.current = JSON.parse(localStorage.getItem("wordList") || "") as wordCard[];
+    }
+  }, []);
+
+
 
   const handleSave = () => {
-    setItem({
-      word: modifiedWord, translation: modifiedTranslation,
-      topic
-    });
+    if (modifiedWord === "" || modifiedTranslation === "" || !topic || !topic.name || !topic.id) {
+      setErrorMessage("Please fill all fields");
+      setNumberClicked(numberClicked + 1);
+      return;
+    }
+    const newWordCard: wordCard = {
+      word: modifiedWord,
+      translation: modifiedTranslation,
+      topic: topic,
+    };
+    wordsList.current.push(newWordCard);
+    localStorage.setItem("wordList", JSON.stringify(wordsList.current));
     setIsOpen(false);
   };
 
   const handleCancel = () => {
-    setModifiedWord(item.word);
-    setModifiedTranslation(item.translation);
     setIsOpen(false);
   };
+  useClickOutside(ref, handleCancel);
 
   return (
-    <div className={`modal ${isOpen ? "block" : "hidden"}`}>
-      <div className="modal-overlay"></div>
-      <div className="modal-content">
-        <div className="modal-header">
-          <h2 className="modal-title">Edit Word Translation</h2>
-        </div>
-        <div className="modal-body">
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="word">
-              Word
-            </label>
+    <div className="absolute top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
+      <div className="bg-white relative shadow-xl rounded-lg flex flex-col justify-center items-center md:p-10 p-6" ref={ref}>
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl font-bold">
+            <span className="text-[#4992ea]">Add&nbsp;</span>
+            <span style={
+              {
+                textShadow: "0 0 5px #4992ea, 0 0 10px #4992ea, 0 0 15px #4992ea, 0 0 20px #4992ea, 0 0 25px #4992ea, 0 0 30px #4992ea, 0 0 35px #4992ea",
+                animation: "glow 2s ease-in-out infinite alternate",
+              }
+            } className="text-white">NEW&nbsp;</span>
+            <span className="text-red-500">word</span>
+          </h1>
+          <div className="flex flex-col justify-center items-center mt-4">
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="word"
-              type="text"
+              className="border border-gray-400 rounded-lg p-2 m-2"
+              placeholder="French word"
               value={modifiedWord}
               onChange={(e) => setModifiedWord(e.target.value)}
             />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="translation">
-              Translation
-            </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="translation"
-              type="text"
+              className="border border-gray-400 rounded-lg p-2 m-2"
+              placeholder="Translation"
               value={modifiedTranslation}
               onChange={(e) => setModifiedTranslation(e.target.value)}
             />
+            <select
+              className="border border-gray-400 rounded-lg p-2 m-2"
+              onChange={(e) => {
+                const selectedTopic = availableTopics.find((topic) => topic.name === e.target.value);
+                if (selectedTopic) {
+                  setTopic(selectedTopic);
+                }
+              }}
+            >
+              <option value="">Select topic</option>
+              {availableTopics.map((topic) => (
+                <option key={topic.id} value={topic.name}>
+                  {topic.name}
+                </option>
+              ))}
+            </select>
+            <div className="flex justify-center items-center">
+              <button className="border border-gray-400 rounded-lg md:px-4 px-2 py-2 m-2 bg-[#4992ea] text-white hover:bg-[#4992eae9] hover:scale-105"
+                onClick={handleSave}>
+                Save
+              </button>
+              <button onClick={handleCancel}
+                className="border border-gray-400 rounded-lg md:px-4 px-2 py-2 m-2 bg-red-500 text-white hover:bg-red-600 hover:scale-105"
+              >
+                Cancel
+              </button>
+            </div>
+            <motion.div className="text-red-500 absolute md:bottom-5 bottom-2"
+              initial={{ y: 0, }}
+              animate={{ y: [2, 0, -2, 0, 2, -2, 0] }}
+              transition={{ duration: 0.3, }}
+              key={numberClicked}
+            >{errorMessage}</motion.div>
           </div>
         </div>
-        <div className="modal-footer">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            onClick={handleCancel}
-          >
-            Cancel
-          </button>
-        </div>
       </div>
-    </div>
+    </div >
   );
 };
 
